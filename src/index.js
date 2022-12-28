6569ca381593f17ac6712712426da6cae381dac0
tarequem <moatartareque@gmail.com>
2022-12-28 17:28:20 -0500

    initial files

--- src/index.js
+++ src/index.js
@@ -8,8 +8,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+
   </React.StrictMode>
-);
+); 
+//pseudo code
+   //camo category
+    //list of images of all the camos of that category
+      //images clickable, loads up description of how to unlock camo, x to close import PropTypes from 'prop-types'
+    //future: have option to see how to unlock attachments
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
